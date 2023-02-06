98387f3f80de2f6df463cfd92c3edbdc72ff97da
Enes Kaya <eneskaya5261@gmail.com>
2023-02-06 15:37:41 +0300

    feat: connect MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.listen("8800", () => {
-    console.log("Backend is running!")
-})
+  console.log("Backend is running!");
+});
