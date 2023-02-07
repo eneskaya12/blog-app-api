f01e9f69aa15a8d7ef407e5eca1dc8e610593b20
Enes Kaya <eneskaya5261@gmail.com>
2023-02-07 14:12:26 +0300

    feat(routes): create 'login' function

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,25 @@ router.post("/register", async (req, res) => {
 
         const user = await newUser.save();
         res.status(200).json(user);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
         
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(400).json("Wrong Credentials!");
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("Wrong Credentials!");
+        
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+
     } catch (err) {
         res.status(500).json(err);
     }
